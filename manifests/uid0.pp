3261a2c5df83abdd9e28caed02951dfe5a10234b
jpadams <jeremy@puppetlabs.com>
2014-04-30 23:04:33 -0700

    Update uid0.pp

--- manifests/uid0.pp
+++ manifests/uid0.pp
@@ -26,17 +26,16 @@ class compliance::uid0 {
     } else {
       $values = []
     }
-      #notify { "Node ${::fqdn} failed uid0 requirement due to existence of ${::uid0_failures} user(s).":
+    #notify { "Node ${::fqdn} failed uid0 requirement due to existence of ${::uid0_failures} user(s).": }
     $code = upcase($::uid0)
     $check = 'UID0'
     $item = ''
     $type = ''
-    #notify { "code:${code}\tcheck:UID0\t failed uid0 requirement due to existence of ${::uid0_failures} user(s)."
+
     $result = split(template('compliance/ehpas.erb'), ",")
     notify { $result:
-        tag => ['ehpas'],
+      tag => ['ehpas'],
     }
-    #}
 
-    #}
+  #}
 }
